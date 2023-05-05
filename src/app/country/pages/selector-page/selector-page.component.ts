7a7bcbe1b89d75a992b0ba80412cebaee43f8dba
Pedro <pedro.moyano.garcia@gmail.com>
2023-05-05 15:09:48 +0200

    Added FormBUilder and FormGroup

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styleUrls: ['./selector-page.component.css']
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit{
+
+  constructor( private fb : FormBuilder){} 
+  ngOnInit(): void {
+    
+  }
+
+  miFormulario : FormGroup = this.fb.group({
+    
+  })
+
 
 }
